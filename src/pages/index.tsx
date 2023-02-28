7a5be086c9c3071c38e2fa5bd52299569bdaf06b
Sabir Hassan <sabirbk06@gmail.com>
2023-02-28 14:20:59 +0500

    basic skeleton for layout and navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
-  return <div>hello</div>
+import type { NextPage } from 'next'
+import Main from '../components/Main'
+
+const Home: NextPage = () => {
+  return <Main />
 }
+
+export default Home
