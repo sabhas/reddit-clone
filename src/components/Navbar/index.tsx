8269ab8f134ac7eaca8ab6cb9588a77ed4733762
Sabir Hassan <sabirbk06@gmail.com>
2023-03-10 12:17:22 +0500

    add logo to navbar navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,29 @@
-import { Box, Container } from '@chakra-ui/react'
 import React from 'react'
+import { Flex, Image } from '@chakra-ui/react'
 
 const Navbar: React.FC = () => {
   return (
-    <Box bg='white' height='42px' padding='6px 10px'>
-      Game thread?
-    </Box>
+    <Flex
+      bg='white'
+      height='44px'
+      padding='6px 12px'
+      justifyContent={{ md: 'space-between' }}
+    >
+      <Flex
+        align='center'
+        width={{ base: '40px', md: 'auto' }}
+        mr={{ base: 0, md: 2 }}
+        cursor='pointer'
+      >
+        <Image src='/images/redditFace.svg' alt='reddit-logo' height='30px' />
+        <Image
+          display={{ base: 'none', md: 'unset' }}
+          src='/images/redditText.svg'
+          alt='reddit-logo-text'
+          height='46px'
+        />
+      </Flex>
+    </Flex>
   )
 }
 export default Navbar
