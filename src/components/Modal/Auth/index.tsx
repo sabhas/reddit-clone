9603acf59be77f37f7adc84811448e1789fd213d
Sabir Hassan <sabirbk06@gmail.com>
2023-03-10 12:53:35 +0500

    add basic layout for auth modal

---
+++ src/components/Modal/Auth/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure
+} from '@chakra-ui/react'
+
+const AuthModal: React.FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>here is modal body</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+export default AuthModal
--- src/components/Navbar/RightContent/index.tsx
+++ src/components/Navbar/RightContent/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Flex } from '@chakra-ui/react'
 import AuthButtons from './authButtons'
+import AuthModal from '@/components/Modal/Auth'
 
 const RightContent: React.FC = () => {
   return (
     <>
       <Flex justifyContent='space-between' alignItems='center'>
+        <AuthModal />
         <AuthButtons />
       </Flex>
     </>
