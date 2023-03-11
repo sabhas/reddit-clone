daabbcdf6572a3c1cfc6fe6852b75c0f5713c2e2
Sabir Hassan <sabirbk06@gmail.com>
2023-03-11 14:30:53 +0500

    add feature for logging in with google

--- src/components/Modal/Auth/oAuthButtons.tsx
+++ src/components/Modal/Auth/oAuthButtons.tsx
@@ -1,16 +1,18 @@
 import { Button, Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import { auth } from '../../../firebase/clientApp'
 
-type OAuthButtonsProps = {}
+const OAuthButtons: React.FC = () => {
+  const [signInWithGoogle, _, loading, authError] = useSignInWithGoogle(auth)
 
-const OAuthButtons: React.FC<OAuthButtonsProps> = () => {
   return (
     <Flex direction='column' mb={4} width='100%'>
       <Button
         variant='oauth'
         mb={2}
-        // onClick={() => signInWithGoogle()}
-        // isLoading={loading}
+        onClick={() => signInWithGoogle()}
+        isLoading={loading}
       >
         <Image
           src='/images/googlelogo.png'
@@ -21,11 +23,9 @@ const OAuthButtons: React.FC<OAuthButtonsProps> = () => {
         Continue with Google
       </Button>
       <Button variant='oauth'>Some Other Provider</Button>
-      {/* {error && (
-        <Text textAlign='center' fontSize='10pt' color='red' mt={2}>
-          {error}
-        </Text>
-      )} */}
+      <Text textAlign='center' fontSize='10pt' color='red' mt={2}>
+        {authError && authError.message}
+      </Text>
     </Flex>
   )
 }
